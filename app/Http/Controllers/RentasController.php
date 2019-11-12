21fd0e7ee683ebee8545a05fdb6beb92fbff9f78
jose <jmmf@outlook.com>
2019-11-11 22:27:20 -0600

    Tabla rentas

---
+++ app/Http/Controllers/RentasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rentas;
+use Illuminate\Http\Request;
+
+class RentasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Rentas  $rentas
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rentas $rentas)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Rentas  $rentas
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rentas $rentas)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Rentas  $rentas
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rentas $rentas)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Rentas  $rentas
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rentas $rentas)
+    {
+        //
+    }
+}
