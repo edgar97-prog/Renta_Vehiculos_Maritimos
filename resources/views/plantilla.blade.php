9f52c2a60580f757f97b7928d197ee3237cda4fd
misael1254 <escom_97@hotmail.com>
2019-11-15 23:37:38 -0600

    Radio buttons con estilos / catalogo

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
         <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/index.js')}}"></script>
